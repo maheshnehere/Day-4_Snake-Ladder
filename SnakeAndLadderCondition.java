53e1fbbb1183e0b22fe7acfaba3dffea42241c9b
Maheshnehere <maheshnehere93@gmail.com>
2023-02-24 19:10:17 +0530

    Added:The Player then checks for a Option. They are No Play, Ladder or Snake. - Use ((RANDOM)) to check for

---
+++ SnakeAndLadderCondition.java
@@ -0,0 +1,34 @@
+public class SnakeAndLadderCondition {
+	
+    public static void main(String[] args) {
+       
+       
+        int Location = 0 ;
+
+        // The Player rolls the die to get a number between 1 to 6.
+
+        Random random = new Random();
+        int dice = random.nextInt(6)+1;
+        System.out.println("Dice Number is = "+dice);
+
+        //The Player then checks for Option. They are No Play,Ladder or Snake.
+
+        int num = random.nextInt(3 );
+
+        switch (num){
+            case 0 :
+                System.out.println("Ladder");
+                Location += dice;break;
+            case 1 :
+                System.out.println("Snake");
+                Location -= dice;
+                if(Location < 0){
+                    Location = 0;
+                }break;
+
+            default:
+                System.out.println("No Play");
+        }
+        System.out.println("Player Location is = "+Location);
+    }
+}
